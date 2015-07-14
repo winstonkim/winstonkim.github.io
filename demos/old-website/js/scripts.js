ef0866ecc078ad4ccd4ca6f664701cc8b6d71c4f
Winston Kim <winzton.kim@gmail.com>
2015-07-14 00:57:45 -0700

    New website rollout

---
+++ demos/old-website/js/scripts.js
@@ -0,0 +1,103 @@
+$(document).ready(function(){
+	$('#down_icon').click(function(){
+		$(document.body).animate({
+			'scrollTop':   $('#section-2').offset().top
+		}, 1500)
+	});
+	// $("#background").fadeIn(800);
+
+	$.fn.toggleNavigation= function() {
+		$("#container").toggleClass('container-pushed');
+		$("#sidebar").toggleClass('sidebar-open');
+		$("#menu").toggleClass("menu-hidden");
+		if (document.body.style.overflow == 'hidden') {
+			document.body.style.overflow = 'auto';
+			$("#fade").css("display", "none");
+		} else {
+			document.body.style.overflow = 'hidden';
+			window.setTimeout($(this).fadeOverlay, 500);
+		}
+	}
+
+	$.fn.fadeOverlay = function() {
+		$("#fade").addClass("fade-pushed");
+		$("#fade").fadeIn(200).css("display","inline");
+	}
+
+	$.fn.click = function() {
+		document.getElementById('fade').onclick = function() {
+			$(this).toggleNavigation();
+		}
+	}
+
+	$(window).on('scroll', function(){
+		var curPos = $(window).scrollTop();
+		// $('body').css('background-position', '0 -' + curPos * .1 + 'px');
+		fadePanels(curPos);
+	}).scroll();
+
+	function fadePanels(curPos) {
+		var panels = $('.panel');
+
+		for (var i = 0; i < panels.length; i++) {
+			var offsetTop = $(panels[i]).offset().top;
+    var halfPanel = (($(panels[i]).height() + 80) / 3) // half the panel height + padding
+    var offsetHalf = halfPanel + offsetTop;
+
+    $(panels[i]).attr('data-scroll-top' , offsetTop);
+    $(panels[i]).attr('data-scroll-half', offsetHalf);
+    
+    var j = (curPos - offsetHalf) / halfPanel;
+
+    if (curPos > $(panels[i]).data('scroll-half')) {
+    	$(panels[i]).css('opacity', 1 - (j));
+    } else {
+    	$(panels[i]).css('opacity', '1');
+    }
+}
+}
+// waypoints
+var waypoint1 = $('#section-2').waypoint(function(down) {
+	$('#about_title').addClass("animated fadeInDown");
+	$('#about_description').addClass("animated fadeInUp");
+}, {
+	offset: '30%'
+})
+
+var waypoint2 = $('#section-2').waypoint(function(down) {
+	$('#me').addClass("animated bounceIn");
+	setTimeout(function(){
+		$('#graphics').css('width','458px');
+		setTimeout(function(){
+			$('#bubble').addClass('animated fadeIn')
+			$('.bubble').css('display','inline');
+		}, 600);
+		
+	},1000);
+})
+})
+
+// function toggleNavigation() {
+// 	var sidebar = document.getElementById('sidebar');
+// 	document.getElementById('container').classList.toggle('container-pushed');
+// 	sidebar.classList.toggle('sidebar-open');
+// 	document.getElementById('menu').classList.toggle('menu-hidden');
+// 	if (document.body.style.overflow == 'hidden') {
+// 		document.body.style.overflow = 'auto';
+// 		document.getElementById('fade').style.display = 'none';
+// 	} else {
+// 		document.body.style.overflow = 'hidden';
+// 		window.setTimeout(fadeOverlay, 500);
+// 	}
+// }
+
+// function fadeOverlay() {
+// 	document.getElementById('fade').classList.add('fade-pushed');
+// 	document.getElementById('fade').style.display = 'inline';
+// }
+
+// function click() {
+// 	document.getElementById('fade').onclick = function() {
+// 		toggleNavigation();
+// 	}
+// }
---
+++ js/scripts.js
@@ -0,0 +1,145 @@
+$(document).ready(function() {
+	var experience = ['calcentral', 'rescomp', 'koinrides', 'ets'];
+	var currentNum;
+
+	$('body').keydown(function(e) {
+		if (e.keyCode == 39) {
+			nextPage();
+		}
+		else if (e.keyCode == 37) {
+			previousPage();
+		}
+	});
+
+	$('.links a').click(function(event){
+		if ($(this).attr('id') === 'resume'){
+			return;
+		}
+		$('.links a').removeClass('active');
+		$(this).addClass('active');
+	});
+
+	$('.logo').hover(
+		function(){
+			var currentID = $(this).attr('id');
+			$('.logo').addClass('fade');
+			$('#'+currentID).removeClass('fade');
+			revealID = currentID + '-desc';
+			$('#'+revealID).removeClass('invisible');
+		},
+		function(){
+			$('.logo').removeClass('fade');
+			var currentID = $(this).attr('id');
+			revealID = currentID + '-desc';
+			$('#'+revealID).addClass('invisible');
+		}
+	);
+
+	$('.icon').hover(
+		function(){
+			var currentID = $(this).attr('id');
+			$('.icon').addClass('fade');
+			$('#'+currentID).removeClass('fade');
+			revealID = currentID + '-desc';
+			$('#'+revealID).removeClass('invisible');
+		},
+		function(){
+			$('.icon').removeClass('fade');
+			var currentID = $(this).attr('id');
+			revealID = currentID + '-desc';
+			$('#'+revealID).addClass('invisible');
+		}
+	);
+
+	function resetNextPrev(){
+		$('#previous').removeClass('inactive');
+		$('#next').removeClass('inactive');
+		$('#previous').addClass('pointer');
+		$('#next').addClass('pointer');
+
+		if (currentNum == 0){
+			$('#previous').addClass('inactive');
+			$('#previous').removeClass('pointer');
+		}
+		else if (currentNum == 3){
+			$('#next').addClass('inactive');
+			$('#next').removeClass('pointer');
+		}
+	}
+
+	function nextPage(){
+		if (currentNum == 3){
+			return;
+		}
+		$('#calcentral-page').addClass('gone');
+		$('#rescomp-page').addClass('gone');
+		$('#koinrides-page').addClass('gone');
+		$('#ets-page').addClass('gone');
+		currentNum+=1;
+		$('#'+experience[currentNum]+'-left').addClass('push-right');
+		$('#'+experience[currentNum]+'-page').removeClass('gone');
+
+		resetNextPrev();
+	}
+
+	function previousPage(){
+		if (currentNum == 0){
+			return;
+		}
+		$('#calcentral-page').addClass('gone');
+		$('#rescomp-page').addClass('gone');
+		$('#koinrides-page').addClass('gone');
+		$('#ets-page').addClass('gone');
+		$('#'+experience[currentNum]+'-left').removeClass('push-right');
+		$('#'+experience[currentNum-1]+'-left').removeClass('gone');
+		currentNum-=1;
+		$('#'+experience[currentNum]+'-page').removeClass('gone');
+
+		resetNextPrev();
+	}
+
+	$('.logo').click(
+		function(){
+			var currentID = $(this).attr('id');
+			currentNum = experience.indexOf(currentID);
+			for (var i = 0; i < currentNum; i++){
+				revealID = experience[i] + '-left';
+				$('#'+revealID).addClass('gone');
+				$('#'+revealID).addClass('push-right');
+			}
+			revealID = currentID + '-left';
+			$('#'+currentID+'-page').removeClass('gone');
+			$('#'+revealID).addClass('push-right');
+			$('.right').addClass('gone');
+			resetNextPrev();
+		}
+	);
+
+	$('#x').click(
+		function(){
+			$('#calcentral-page').addClass('gone');
+			$('#rescomp-page').addClass('gone');
+			$('#koinrides-page').addClass('gone');
+			$('#ets-page').addClass('gone');
+			$('.left-ex').removeClass('push-right');
+			$('.right').removeClass('gone');
+			$('.left-ex').removeClass('gone');
+		}
+	);
+
+	$('#next').click(nextPage);
+	$('#previous').click(previousPage);
+
+	$('#experience-mobile').click(function( e ){  
+	    e.preventDefault();
+	    var top = $("#calcentral-left").offset().top;
+	    $('html, body').stop().animate({scrollTop: top }, 1500);
+	});
+
+	$('#contact-mobile').click(function( e ){  
+	    e.preventDefault();
+	    var top = $("#contact").offset().top;
+	    $('html, body').stop().animate({scrollTop: top }, 1500);
+	});
+
+});
